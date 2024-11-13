4c6edd50398bd286579b13fc342d7e320c743706
Nicoly Ott <nicolyott23@gmail.com>
2024-11-13 08:23:31 -0300

    feat: first class: position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Position position = new Position(3,5);
+
+        System.out.println(position);
+
     }
 }
